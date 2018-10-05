08b84ec3079a7299e1026910a1ca2f4f79e67d7d
Joohyun Lee <dlee110600@gmail.com>
2018-10-05 13:51:21 +0900

    Create crawling.py

---
+++ data/crawling.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import os
+import urllib
+import pdb
+
+
+LABEL_PATH = './data/label/label.xls'
+IMAGE_ROOT = './data/image'
+
+def download(data_path, output_root):
+    xls = pd.read_excel(data_path)
+    for i in range(len(xls['link'])):
+        print(i)
+        link = xls['link'][i]
+        name = xls['No'][i]
+        img_name = os.path.join(output_root, str(name)) + '.jpg'
+        if not(isinstance(link, str)):
+            continue
+        if link.split(':')[0] == 'http':
+            urllib.request.urlretrieve(link, img_name)
+
+
+download(LABEL_PATH, IMAGE_ROOT)
