89e8f43166e6a2c9dd5fc23c258f349a8123ae55
Joohyun Lee <dlee110600@gmail.com>
2018-10-06 00:21:38 +0900

    Update train.py

--- train.py
+++ train.py
@@ -19,42 +19,58 @@ def train(args):
 
     # Data Load
     trainloader = data_loader(args, mode='TRAIN')
-    testloader = data_loader(args, mode='TEST')
+    validloader = data_loader(args, mode='VALID')
 
     # Model Load
-    resnet, optimizer, best_loss, start_epoch = load_model(args, class_num=11, mode='TRAIN')
+    resnet, optimizer, best_loss, start_epoch =\
+        load_model(args, mode='TRAIN')
 
     # Loss Init
     for epoch in range(start_epoch, args.epochs + 1):
         # Train Model
-        print('Epoch: {}\n  >> Train\n'.format(epoch))
+        print('Epoch: {}\n  >> Train'.format(epoch))
         resnet.train(True)
         loss = 0
+        acc = 0
         lr = args.lr * (0.1 ** (epoch // 30))
         for param_group in optimizer.param_groups:
             param_group["lr"] = lr
-        for idx, (img, shape, color1, color2, img_path) in enumerate(trainloader):
-            img, shape = img.to(device), shape.to(device)
+        for idx, (img, label, img_path) in enumerate(trainloader):
+            img, label = img.to(device), label.to(device)
             output = resnet(img)
-            batch_loss = F.cross_entropy(output, shape)
+            batch_loss = F.cross_entropy(output, label)
             optimizer.zero_grad()
             batch_loss.backward()
             optimizer.step()
-            loss += float(batch_loss)
-            progress_bar(idx, len(trainloader), 'Loss: %.5f'%((loss/(idx+1))))
 
-        print('\n  >>Validation\n')
+            output = (output.cpu().detach().numpy()).argmax(axis=1)
+            label = label.cpu().detach().numpy()
+            total = output.shape[0]
+            correct = (output == label).astype(np.int).sum()
+            acc += float(correct/total)
+            loss += float(batch_loss)
+            progress_bar(idx, len(trainloader), 'Loss: %.5f, Acc: %.5f'
+                    %((loss/(idx+1)), acc/(idx+1)))
+
+        print('\n\n  >>Validation')
         resnet.eval()
         loss = 0
-        for idx, (img, shape, color1, color2, img_path) in enumerate(trainloader):
-            img, shape = img.to(device), shape.to(device)
+        acc = 0
+        for idx, (img, label, img_path) in enumerate(trainloader):
+            img, label = img.to(device), label.to(device)
             output = resnet(img)
+            output = (output.cpu().detach().numpy()).argmax(axis=1)
+            label = label.cpu().detach().numpy()
+            total = output.shape[0]
+            correct = (output == label).astype(np.int).sum()
+            acc += float(correct/total)
             loss += float(batch_loss)
-            progress_bar(idx, len(trainloader), 'Loss: %.5f'%((loss/(idx+1))))
+            progress_bar(idx, len(trainloader), 'Loss: %.5f, Acc: %.5f'
+                    %((loss/(idx+1)), acc/(idx+1)))
 
         loss /= (idx+1)
         if loss < best_loss:
-            checkpoint = Checkpoint(resnet, optimizer, epoch, score)
+            checkpoint = Checkpoint(resnet, optimizer, epoch, loss)
             checkpoint.save(args.ckpt_path)
             best_loss = loss
             print("Saving...")
@@ -64,6 +80,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument("--resume", type=bool, default=False,
                         help="Model Trianing resume.")
+    parser.add_argument("--data", type=str, default="shape",
+                        help="data to train(shape, color1, color2)")
     parser.add_argument("--batch_size", type=int, default=12,
                         help="The batch size to load the data")
     parser.add_argument("--epochs", type=int, default=150,
